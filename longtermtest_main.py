4fa1ee98174f04d60fc7426fcfc84ef29223da39
adhitya-spas <adhityashreyas.p@gmail.com>
2024-08-22 11:03:04 -0600

    Fixed timing issue with signal generators

--- longtermtest_main.py
+++ longtermtest_main.py
@@ -28,9 +28,9 @@ sine = "sine"
 
 # --------------------------------------------------------------------------------------------------
 ### MACROS for Testing (1 -> True, 0 -> False)
-FIXED_FREQ  = 2                 # Set to 1 if you want to set frequencies || Set to 0 if you want random frequencies || Set to 2 if you want a step-wise frequency change
-FIXED_AMP   = 2                 # Set to 1 if you want to set amplitude || Set to 0 if you want random amplitude
-FIXED_PHASE = 0                 # Set to 1 if you want to set phase || Set to 0 if you want random phase
+FIXED_FREQ  = 1#2                 # Set to 1 if you want to set frequencies || Set to 0 if you want random frequencies || Set to 2 if you want a step-wise frequency change
+FIXED_AMP   = 1#2                 # Set to 1 if you want to set amplitude || Set to 0 if you want random amplitude
+FIXED_PHASE = 1#0                 # Set to 1 if you want to set phase || Set to 0 if you want random phase
 
 RAW_DATA    = 1                 # Set to 1 if you want packets saved with "\n" || Set to 0 if you want raw data
 # --------------------------------------------------------------------------------------------------
@@ -49,7 +49,7 @@ set_amp     = [low_amp, low_amp, low_amp, low_amp, low_amp]      # [Ch1, Ch2, Ch
 start_phase = 0         # deg
 end_phase   = 180       # deg
 step_phase  = 1         # deg
-set_phase   = [0, 3, 6, 73, 16]     # [Ch1, Ch2, Ch3, Ch4, Ch5] || FOR FIXED PHASE, line 110
+set_phase   = [0, 32, 46, 73, 16]     # [Ch1, Ch2, Ch3, Ch4, Ch5] || FOR FIXED PHASE, line 110
 
 switch_time = 10        # seconds
 switch_count= 0         # A counter for FIXED_FREQ=2; when to switch between testing sets 
@@ -255,21 +255,21 @@ while(True):
         # Available Amplitudes
         amp_list = np.arange(start = low_amp, stop = hi_amp, step = step_amp).tolist()
         
-	if switch_count == 2:
-		if counter == -1:
-			# Choosing Amplitudes (Vpp)
-        		amp1 = random.choice(amp_list)
-        		amp2 = random.choice(amp_list)
-        		amp3 = random.choice(amp_list)
-        		amp4 = random.choice(amp_list)
-        		amp5 = random.choice(amp_list)
-	else:
-        	# Choosing Amplitudes (Vpp)
-        	amp1 = random.choice(amp_list)
-        	amp2 = random.choice(amp_list)
-        	amp3 = random.choice(amp_list)
-        	amp4 = random.choice(amp_list)
-        	amp5 = random.choice(amp_list)
+        if switch_count == 2:
+            if counter == -1:
+                # Choosing Amplitudes (Vpp)
+                amp1 = random.choice(amp_list)
+                amp2 = random.choice(amp_list)
+                amp3 = random.choice(amp_list)
+                amp4 = random.choice(amp_list)
+                amp5 = random.choice(amp_list)
+            else:
+                # Choosing Amplitudes (Vpp)
+                amp1 = random.choice(amp_list)
+                amp2 = random.choice(amp_list)
+                amp3 = random.choice(amp_list)
+                amp4 = random.choice(amp_list)
+                amp5 = random.choice(amp_list)
 
     if FIXED_AMP == 1:
         # Manually set amplitude in Line 41
@@ -335,25 +335,31 @@ while(True):
     SG2025_1.write("*idn?") 
 
     # Configure to output sine wave
-    time.sleep(0.8)
+    time.sleep(0.1)
     print("Setting up Channel 1")
     SG2025_1.write("C1:BSWV WVTP,SINE")
+    time.sleep(0.1)
     SG2025_1.write("C1:BSWV FRQ,",str(freq1))
+    time.sleep(0.1)
     SG2025_1.write("C1:BSWV AMP,",str(amp1))
+    time.sleep(0.1)
     SG2025_1.write("C1:BSWV PHSE,",str(phase1))
     SG2025_1.write("C1:OUTP ON")
     print("\t : COMPLETED")
 
-    time.sleep(0.8)
+    time.sleep(0.1)
     print("Setting up Channel 2")
     SG2025_1.write("C2:BSWV WVTP,SINE")
+    time.sleep(0.1)
     SG2025_1.write("C2:BSWV FRQ,",str(freq2))
+    time.sleep(0.1)
     SG2025_1.write("C2:BSWV AMP,",str(amp2))
+    time.sleep(0.1)
     SG2025_1.write("C2:BSWV PHSE,",str(phase2))
     SG2025_1.write("C2:OUTP ON")
     print("\t : COMPLETED")
 
-
+    
     ## Open Signal Generator 2 (In the middle of the stack)
     print("Setting up Signal Generator 2")
     SG2025_2 = rm.open_resource('USB0::0xF4ED::0xEE3A::SDG10GAQ1R1236::INSTR') # confirm parameters
@@ -363,20 +369,26 @@ while(True):
     SG2025_2.write("*idn?") 
 
     # Configure to output sine wave
-    time.sleep(0.8)
+    time.sleep(0.1)
     print("Setting up Channel 3")
     SG2025_2.write("C1:BSWV WVTP,SINE")
+    time.sleep(0.1)
     SG2025_2.write("C1:BSWV FRQ,",str(freq3))
+    time.sleep(0.1)
     SG2025_2.write("C1:BSWV AMP,",str(amp3))
+    time.sleep(0.1)
     SG2025_2.write("C1:BSWV PHSE,",str(phase3))
     SG2025_2.write("C1:OUTP ON")
     print("\t : COMPLETED")
 
-    time.sleep(0.8)
+    time.sleep(0.1)
     print("Setting up Channel 4")
     SG2025_2.write("C2:BSWV WVTP,SINE")
+    time.sleep(0.1)
     SG2025_2.write("C2:BSWV FRQ,",str(freq4))
+    time.sleep(0.1)
     SG2025_2.write("C2:BSWV AMP,",str(amp4))
+    time.sleep(0.1)
     SG2025_2.write("C2:BSWV PHSE,",str(phase4))
     SG2025_2.write("C2:OUTP ON")
     print("\t : COMPLETED")
@@ -391,11 +403,14 @@ while(True):
     SG2025_3.write("*idn?") 
 
     # Configure to output sine wave
-    time.sleep(0.8)
+    time.sleep(0.1)
     print("Setting up Channel 5")
     SG2025_3.write("C1:BSWV WVTP,SINE")
+    time.sleep(0.1)
     SG2025_3.write("C1:BSWV FRQ,",str(freq5))
+    time.sleep(0.1)
     SG2025_3.write("C1:BSWV AMP,",str(amp5))
+    time.sleep(0.1)
     SG2025_3.write("C1:BSWV PHSE,",str(phase5))
     SG2025_3.write("C1:OUTP ON")
     print("\t : COMPLETED")
@@ -430,3 +445,4 @@ while(True):
     ### Wait time for next signal transition
     time.sleep(switch_time)     # switch_time can be set up in the MACRO SECTION in Line 53
 
+# SG2025_1.write("C1:OUTP OFF")
